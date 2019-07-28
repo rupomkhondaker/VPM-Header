181c8836eb724535fc9f41ccefafd21eacd19398
Rupom Khondaker <rupomkhondaker@users.noreply.github.com>
2019-07-28 06:08:43 +0600

    1

--- js/custom.js
+++ js/custom.js
@@ -1,67 +1,66 @@
-  jQuery(document).ready(function(){
-		$(".dropdown").hover(            
-			function() {
-				$('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideDown("600");
-				$(this).toggleClass('open');        
-			},
-			function() {
-				$('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideUp("300");
-				$(this).toggleClass('open');       
-			}
-		);
-	
-		var menuPos = $('.header-desktop-bottom').offset().top;
-		//var adminMenuHeight = $('#wpadminbar').height(); {"backgroundColor": "black", "color": "white"}
-		$(window).bind('scroll', function () {	
-			$(window).scroll(function(){
-				if ($(window).scrollTop() >= 50) {
-					$('.header-desktop-bottom,.header-mobile-bottom').addClass('fixed-header');
-					$('.main-logo').hide();
-					$('.sticky-logo').show();
-					$('#menu-icon-scroll-search').css({"margin-top":"13px", "display": "block"}).show();
-					$('#menu-icon-no-scroll-search').hide();
-					$('#menu-icon-scroll-menu').css('margin-top',"13px").show();
-					$('#menu-icon-no-scroll-menu').hide();
-				}
-				else {
-					$('.header-desktop-bottom').removeClass('fixed-header');
-					$('.header-mobile-bottom').removeClass('fixed-header');
-					$('.sticky-logo').hide();
-					$('.main-logo').show();
-					$('#menu-icon-no-scroll-search').show();
-					$('#menu-icon-scroll-search').hide();
-					$('#menu-icon-no-scroll-menu').show();
-					$('#menu-icon-scroll-menu').hide();
-				}
-			});
-		});
-	
-		$("#VPMTicker").breakingNews({
-			effect		:"fade",
-			autoplay	:true,
-			timer		:5000,
-			effectspeed	:1000
-		});
-		window.onload = function(e) {
-			  setTimeout(function(){ 
-					$("#VPMTicker").fadeIn('slow').show();
-			  }, 5000);
-        };
-		//Mobile menu toggleClass
-		var $main_nav = $('#main-nav');
-          var $toggle = $('.toggle');
-          var defaultData = {
-            maxWidth: false,
-            customToggle: $toggle,
-            navTitle: 'Vape Parts Mart',
-            levelTitles: true,
-            insertClose: 2,
-            closeLevels: false
-          };
-          // call our plugin
-          var Nav = $main_nav.hcOffcanvasNav(defaultData);
+jQuery(document).ready(function(){
+	$(".dropdown").hover(            
+		function() {
+			$('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideDown("600");
+			$(this).toggleClass('open');        
+		},
+		function() {
+			$('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideUp("300");
+			$(this).toggleClass('open');       
+		}
+	);
 
+	var menuPos = $('.header-desktop-bottom').offset().top;
+	//var adminMenuHeight = $('#wpadminbar').height(); {"backgroundColor": "black", "color": "white"}
+	$(window).bind('scroll', function () {	
+		$(window).scroll(function(){
+			if ($(window).scrollTop() >= 50) {
+				$('.header-desktop-bottom,.header-mobile-bottom').addClass('fixed-header');
+				$('.main-logo').hide();
+				$('.sticky-logo').show();
+				$('#menu-icon-scroll-search').css({"margin-top":"13px", "display": "block"}).show();
+				$('#menu-icon-no-scroll-search').hide();
+				$('#menu-icon-scroll-menu').css('margin-top',"13px").show();
+				$('#menu-icon-no-scroll-menu').hide();
+			}
+			else {
+				$('.header-desktop-bottom').removeClass('fixed-header');
+				$('.header-mobile-bottom').removeClass('fixed-header');
+				$('.sticky-logo').hide();
+				$('.main-logo').show();
+				$('#menu-icon-no-scroll-search').show();
+				$('#menu-icon-scroll-search').hide();
+				$('#menu-icon-no-scroll-menu').show();
+				$('#menu-icon-scroll-menu').hide();
+			}
 		});
+	});
+
+	$("#VPMTicker").breakingNews({
+		effect		:"fade",
+		autoplay	:true,
+		timer		:5000,
+		effectspeed	:1000
+	});
+	window.onload = function(e) {
+		  setTimeout(function(){ 
+				$("#VPMTicker").fadeIn('slow').show();
+		  }, 5000);
+	};
+	//Mobile menu toggleClass
+	var $main_nav = $('#main-nav');
+	  var $toggle = $('.toggle');
+	  var defaultData = {
+		maxWidth: false,
+		customToggle: $toggle,
+		navTitle: 'Vape Parts Mart',
+		levelTitles: true,
+		insertClose: 2,
+		closeLevels: false
+	  };
+	  // call our plugin
+	  var Nav = $main_nav.hcOffcanvasNav(defaultData);
+});
   
   /*//fixed navigation on top when scrolling
 			if (screen && screen.width > 768) {
